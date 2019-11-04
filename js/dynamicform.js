e7b19e8371acdf9ede1f2e65951980b362e19979
Phil Asmar <phil.asmar@gmail.com>
2019-11-04 14:59:35 -0500

    Added dynamic form logging

--- js/dynamicform.js
+++ js/dynamicform.js
@@ -3,9 +3,15 @@ currentQuestion = 1;
 function registerKey(e) {
 	var code = (e.keyCode ? e.keyCode : e.which);
 	if(code == 13) { //Enter keycode
+
+		var question = {
+			id: $(".formQuestion").attr("questionid"),
+			answer: $(".formQuestion").find("input").val()
+		}
+		firebase.database().ref('nopioid-dynamic-form-results').push().set(question);
 		$('.formQuestion').remove();
 		currentQuestion += 1;
-  	$('#questionArea').append("<div class='formQuestion'><h2>" + currentQuestion + ". " + questionArr[currentQuestion - 1]+ "</h2><input onkeydown='registerKey(event)' class='lui-input  lui-input--large'/></div>");
+  	$('#questionArea').append("<div class='formQuestion' questionid='" + currentQuestion + "'><h2>" + currentQuestion + ". " + questionArr[currentQuestion - 1]+ "</h2><input onkeydown='registerKey(event)' class='lui-input  lui-input--large'/></div>");
 		$('.formQuestion>input').select();
 	}
 }
@@ -16,6 +22,6 @@ firebase.database().ref('nopioid-dynamic-form-questions/').orderByChild('questio
 	for (x in json) {
 	  questionArr.push(json[x].question);
 	}
-	$('#questionArea').append("<div class='formQuestion'><h2>1. " + questionArr[0]+ "</h2><input onkeydown='registerKey(event)' class='lui-input  lui-input--large'/></div>");
+	$('#questionArea').append("<div class='formQuestion' questionid='" + 1 + "'><h2>1. " + questionArr[0]+ "</h2><input onkeydown='registerKey(event)' class='lui-input  lui-input--large'/></div>");
 	$('.formQuestion>input').select();
 });
